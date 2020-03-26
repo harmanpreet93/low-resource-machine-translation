9030445309dd15fd13ddfaae2badd221142e6140
Alex Peplowski <alexander.peplowski@gmail.com>
2020-03-26 12:16:40 -0400

    Fixed rsync_data.sh

--- scripts/rsync_data.sh
+++ scripts/rsync_data.sh
@@ -1,1 +1,1 @@
-rsync -avzh guest139@helios3.calculquebec.ca:/project/cq-training-1/project1/teams/team08/data ..
+rsync -avzh guest139@helios3.calculquebec.ca:/project/cq-training-1/project2/data ..
