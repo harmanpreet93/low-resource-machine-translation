5ef177dcb1b0502f7fb990e14c645a4256d6a903
Alex <alexander.peplowski@gmail.com>
2020-04-07 18:33:08 +0000

    Fixed scripts for running on server, note: no downloading available on compute node

--- scripts/run_tokenizer_en.sh
+++ scripts/run_tokenizer_en.sh
@@ -1,2 +1,2 @@
- python -m spacy download en_core_web_sm
+ #python -m spacy download en_core_web_sm
  python ../code/tokenizer.py --input ../data/unaligned.en --output ../data/train.en.tokenized --lang en
--- scripts/run_tokenizer_fr.sh
+++ scripts/run_tokenizer_fr.sh
@@ -1,2 +1,2 @@
- python -m spacy download fr_core_news_sm
+ #python -m spacy download fr_core_news_sm
  python ../code/tokenizer.py --input ../data/unaligned.fr --output ../data/train.fr.tokenized --lang fr # --keep-case
