280e404b6283067b4058588e703f1a12e6974ed1
Harmanpreet Singh <singh.harmanpreet93@gmail.com>
2020-04-08 03:15:42 -0400

    run language modelling code added, custom tokenizer file added

--- code/custom_tokenizer_trainer.py
+++ code/custom_tokenizer_trainer.py
@@ -1,13 +1,15 @@
 import os
 import json
 import tokenizers
+import argparse
+import sys
 
 
 class CustomTokenizerTrainer():
     """CustomTokenizerTrainer based on ByteLevelBPETokenizer"""
 
     def __init__(self, save_tokenizer_path: str,
-                 training_files: str,
+                 training_files,
                  special_tokens,
                  min_frequency: int,
                  lowercase: bool,
@@ -22,6 +24,7 @@ class CustomTokenizerTrainer():
         self.tokenizer = tokenizers.ByteLevelBPETokenizer(lowercase=self.lowercase)
 
     def train(self):
+
         self.tokenizer.train(
             files=self.training_files,
             vocab_size=self.VOCAB_SIZE,
@@ -38,43 +41,87 @@ class CustomTokenizerTrainer():
 
         model_type = "roberta"  # roBERTa model is better than BERT for language modelling
         config = {
-            "vocab_size": self.VOCAB_SIZE,
-            "model_type": model_type
+            "architectures": [
+                "RobertaForMaskedLM"
+            ],
+            "attention_probs_dropout_prob": 0.1,
+            "hidden_act": "gelu",
+            "hidden_dropout_prob": 0.1,
+            "hidden_size": 768,
+            "initializer_range": 0.02,
+            "intermediate_size": 3072,
+            "layer_norm_eps": 1e-05,
+            "max_position_embeddings": 514,
+            "model_type": model_type,
+            "num_attention_heads": 12,
+            "num_hidden_layers": 6,
+            "type_vocab_size": 1,
+            "vocab_size": self.VOCAB_SIZE
         }
 
         config_path = os.path.join(self.save_tokenizer_path, "config.json")
         with open(config_path, 'w') as fp:
             json.dump(config, fp)
 
+        tokenizer_config = {
+            "max_len": 512
+        }
+
+        config_path = os.path.join(self.save_tokenizer_path, "tokenizer_config.json")
+        with open(config_path, 'w') as fp:
+            json.dump(tokenizer_config, fp)
+
+        special_tokens_map = {"bos_token": "<s>", "eos_token": "</s>", "unk_token": "<unk>", "sep_token": "</s>",
+                              "pad_token": "<pad>", "cls_token": "<s>", "mask_token": "<mask>"}
+
+        config_path = os.path.join(self.save_tokenizer_path, "special_tokens_map.json")
+        with open(config_path, 'w') as fp:
+            json.dump(special_tokens_map, fp)
+
+        # with open("data/merged_data.en", 'w', encoding="utf-8") as fout:
+        #     for file in self.training_files:
+        #         with open(file, 'r') as fin:
+        #             for line in fin.readlines():
+        #                 fout.write(line)
+
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 
 def main():
-    # how to train a tokenizer
-    data_path = "../../../machine-translation/data"
-    VOCAB_SIZE = 60000
-    min_frequency = 2
-    lowercase = True
-    special_tokens = ["<s>", "<pad>", "</s>", "<unk>", "<mask>"]
-    unaligned_en_path = os.path.join(data_path, 'unaligned.en')
-    aligned_en_path = os.path.join(data_path, 'train.lang1')
-    training_files = [unaligned_en_path, aligned_en_path]
-    pretrained_tokenizer_path = os.path.join(data_path, 'tokenizer_data')
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--vocab_size", default=60000, help="Vocabulary Size", type=int)
+    parser.add_argument("--lowercase", type=str2bool, default="False")
+    parser.add_argument("--min_frequency", default=2, type=int,
+                        help="Minimum frequency to consider while training tokenizer")
+    parser.add_argument("--training_folder", default=None, help="Folder containing files for tokenizer")
+    parser.add_argument("--path_to_save_tokenizer", default=None, help="Folder to save tokenizer files")
+    args = parser.parse_args()
+    print("Args: ", sys.argv[1:])
+
+    special_tokens = ["<pad>", "<s>", "</s>", "<unk>", "<mask>"]
+
+    training_files = [os.path.abspath(os.path.join(args.training_folder, x)) for x in os.listdir(args.training_folder)]
 
     # create custom tokenizer
-    tokenizerObj = CustomTokenizerTrainer(pretrained_tokenizer_path,
-                                          training_files,
-                                          special_tokens,
-                                          min_frequency,
-                                          lowercase,
-                                          VOCAB_SIZE)
+    train_tokenizer_obj = CustomTokenizerTrainer(args.path_to_save_tokenizer,
+                                                 training_files,
+                                                 special_tokens,
+                                                 args.min_frequency,
+                                                 args.lowercase,
+                                                 args.vocab_size)
 
-    ALREADY_TRAINED_ONCE = False
-    if ALREADY_TRAINED_ONCE:
-        print("Tokenizer already trained. Set ALREADY_TRAINED_ONCE=False to re-train tokenizer")
-    else:
-        # train your Byte Pair Tokenizer on your own training files!
-        tokenizerObj.train()
+    # train your Byte Pair Tokenizer on your own training files!
+    train_tokenizer_obj.train()
 
-    ##############################################################
+    '''
     # how to load the saved tokenizer
     from transformers import AutoTokenizer
 
@@ -84,6 +131,7 @@ def main():
     # sample usage
     text = "Montreal is a great city".strip().lowercase()
     tokenizer.tokenize(text)
+    '''
 
 
 if __name__ == "__main__":
---
+++ scripts/train_custom_tokenizer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --time=10:00
+#SBATCH --gres=gpu:k80:1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=4G
+#SBATCH --reservation=IFT6759
+
+module load python/3.7
+source /home/guest140/harman_venv/bin/activate
+# source /home/guest164/ift6759-env/bin/activate
+
+date
+echo ~~~~~~~~~~~~Tokenizing data
+echo
+
+python custom_tokenizer_trainer.py \
+	--vocab_size 60000 \
+	--lowercase True \
+	--min_frequency 2 \
+	--training_folder ./tokenizer_train_folder \
+	--path_to_save_tokenizer ./tokenizer_data \
