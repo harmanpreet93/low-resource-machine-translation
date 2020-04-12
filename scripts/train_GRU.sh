f7208d627e40add64e2470d4d8e2436f5ddfcbe4
Marc-Antoine Provost <44839792+MarcAnProv@users.noreply.github.com>
2020-04-12 10:09:42 -0400

    GRU files

---
+++ scripts/train_GRU.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --time=240:00
+#SBATCH --gres=gpu:k80:1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G
+
+module load python/3.7
+source /home/guest141/test_env/bin/activate
+
+date
+echo ~~~~~~~~~~~~Training GRU model
+echo
+
+python -u /home/guest141/GRU.py --config /home/guest141/data_project2/GRU_config.json \
+
+date
+echo ~~~~~~~~~~~~finished
