3887debe199df454168fe52424fb301736891261
Number One Hundred Forty Two Guest <guest142@helios3.helios.calculquebec.ca>
2020-04-18 18:41:02 +0000

    ML Bidirectional LSTM+ELMo

---
+++ code/ELMo_scripts/dump_weights_fr.sh
@@ -0,0 +1,5 @@
+
+source /home/guest142/env2/bin/activate
+
+python bin/dump_weights.py --save_dir '/project/cq-training-1/project2/teams/team08/ELMo/swb_fr/checkpoint' --outfile '/project/cq-training-1/project2/teams/team08/ELMo/swb_fr/swb_weights_fr.hdf5'
+
