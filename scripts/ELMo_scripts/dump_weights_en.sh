f324779c77b7e2d5032a6570da31c2545208c195
Number One Hundred Forty Two Guest <guest142@helios3.helios.calculquebec.ca>
2020-04-18 18:53:37 +0000

    put elmo scripts on pretrained_embeddins folder

--- scripts/ELMo_scripts/dump_weights_en.sh
+++
@@ -1,5 +0,0 @@
-
-source /home/guest142/env2/bin/activate
-
-python bin/dump_weights.py --save_dir '/project/cq-training-1/project2/teams/team08/ELMo/swb_en2/checkpoint' --outfile '/project/cq-training-1/project2/teams/team08/ELMo/swb_en2/swb_weights_en2.hdf5'
-
