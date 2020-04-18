c152240730ee86b6e26c36e2f42d6133390784c3
Marc-Antoine Provost <44839792+MarcAnProv@users.noreply.github.com>
2020-04-17 22:16:58 -0400

    GRU

--- scripts/train_GRU.sh
+++ scripts/train_GRU.sh
@@ -11,7 +11,7 @@ date
 echo ~~~~~~~~~~~~Training GRU model
 echo
 
-python -u /home/guest141/GRU.py --config /home/guest141/data_project2/GRU_config.json \
+python -u /home/guest141/GRU.py --config /home/guest141/data_project2/GRU_config.json --embedding_model Word2Vec\
 
 date
 echo ~~~~~~~~~~~~finished
